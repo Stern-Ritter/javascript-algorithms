c9efc97d5ed9d2f308772453ed6a7bb62657e41f
Stern-Ritter <fadeew.a@mail.ru>
2021-09-01 22:53:06 +0300

    Tasks for review

--- 08-fibonacci/script.js
+++ 08-fibonacci/script.js
@@ -3,15 +3,29 @@
  *
  * Последовательность Фибоначчи — это порядок чисел, где каждое последующее
  * число является суммой двух предыдущих: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
- * 
+ *
  * Напишите функцию, которая принимает на вход число n и возвращает n-й элемент
  * последовательности Фибоначчи.
-*/
+ */
 
 function fibonacci(n) {
-    // Напишите код здесь
+  if (typeof n !== 'number' || n <= 1) {
+    return 0;
+  } else {
+    let previous = 0;
+    let current = 1;
+    for (let i = 3; i <= n; i++) {
+      current += previous;
+      previous = current - previous;
+    }
+    return current;
+  }
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(fibonacci(4)); // 2. Четвёртое число последовательности — двойка (0, 1, 1, 2)
+console.log(fibonacci(-99)); // 0
+console.log(fibonacci('a')); // 0
+console.log(fibonacci('')); // 0
+console.log(fibonacci()); // 0
